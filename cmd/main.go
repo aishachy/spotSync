6dde68f9657f6c158a9b787b9d345a5dd7a858b6
Aysha Aziz <aishaazizz30@gmail.com>
2026-06-27 20:41:00 +0400

    gorm model setup and basic create users created

---
+++ cmd/main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/go-playground/validator/v10"
+	"github.com/labstack/echo/v5"
+	"github.com/labstack/echo/v5/middleware"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+type User struct {
+	gorm.Model
+	Name     string `json:"name" validate:"required" gorm:"type:varchar(100); not null"`
+	Email    string `json:"email" validate:"required,email" gorm:"type:varchar(255); uniqueIndex:not null"`
+	Password string `json:"password" validate:"required,min=6" gorm:"type:varchar(100); not null"`
+}
+
+type CustomValidator struct {
+	validator *validator.Validate
+}
+
+func (cv *CustomValidator) Validate(i any) error {
+	if err := cv.validator.Struct(i); err != nil {
+		return echo.ErrBadRequest.Wrap(err)
+	}
+	return nil
+}
+
+func main() {
+
+	dsn := "host=localhost user=postgres password=postgres dbname=spotSync port=5432 sslmode=disable TimeZone=Asia/Shanghai"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
+		TranslateError: true,
+	})
+
+	if err != nil {
+		panic(err)
+	}
+
+	db.AutoMigrate(&User{})
+
+	e := echo.New()
+	e.Validator = &CustomValidator{validator: validator.New()}
+
+	e.Use(middleware.RequestLogger())
+	e.Use(middleware.Recover())
+
+	e.GET("/", func(c *echo.Context) error {
+		return c.JSON(http.StatusOK, map[string]string{"message": "Hello, World!"})
+	})
+
+	e.POST("/users", func(c *echo.Context) error {
+		newUser := new(User)
+		if err := c.Bind(newUser); err != nil {
+			return err
+		}
+
+		if err := c.Validate(newUser); err != nil {
+			return c.JSON(http.StatusBadRequest, map[string]any{"error": err.Error()})
+		}
+
+		result := db.Create(newUser)
+		if result.Error != nil {
+			return c.JSON(http.StatusInternalServerError, map[string]any{"error": result.Error.Error()})
+		}
+
+		return c.JSON(http.StatusCreated, newUser)
+	})
+
+	if err := e.Start(":5000"); err != nil {
+		e.Logger.Error("failed to start server", "error", err)
+	}
+}
